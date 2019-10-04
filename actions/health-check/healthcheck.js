c5505a99062d757b3739164a01f05ef98ffc0a3e
Keen <Keenwby@users.noreply.github.com>
2019-10-03 19:40:09 -0700

    Update healthcheck.js

--- actions/health-check/healthcheck.js
+++ actions/health-check/healthcheck.js
@@ -3,8 +3,11 @@ const github = require('@actions/github');
 
 try {
   const condition = core.getInput('condition');
-  const passingStatus = core.getInput('passing_status');
-  console.log(`Heathcheck of condition $condition, passing_status $passing_status`);
+  const passingStatus = core.getInput('passing_status'); 
+  const fabricNames = core.getInput('fabric_names');
+  const tags = core.getInput('tags');
+  console.log(`Heathcheck on fabrics ${fabricNames}, tags ${tags}`);
+  console.log(`Heathcheck of condition ${condition}, passing_status ${passingStatus}`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   core.setOutput("status", "SUCCEEDED");
