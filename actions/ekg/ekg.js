7209d570c86036be2b08db8b1a6b3d37a272af5b
Keen <Keenwby@users.noreply.github.com>
2019-10-04 14:36:41 -0700

    Update ekg.js

--- actions/ekg/ekg.js
+++ actions/ekg/ekg.js
@@ -2,9 +2,8 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const condition = core.getInput('condition');
-  const passingStatus = core.getInput('passing_status');
-  console.log(`EKG of condition ${condition}, passing_status ${passingStatus}`);
+  const criteria = core.getInput('criteria');
+  console.log(`EKG of criteria ${criteria}`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   core.setOutput("status", "SUCCEEDED");
