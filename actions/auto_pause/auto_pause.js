c3c1f49352bbb829bf1cbd9c3000a0abb7a84c71
Keen <Keenwby@users.noreply.github.com>
2019-10-04 14:18:00 -0700

    Update and rename healthcheck.js to auto_pause.js

--- actions/auto_pause/auto_pause.js
+++ actions/auto_pause/auto_pause.js
@@ -2,9 +2,9 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const condition = core.getInput('condition');
-  const passingStatus = core.getInput('passing_status');
-  console.log(`Heathcheck of condition $condition, passing_status $passing_status`);
+  const duration = core.getInput('duration');
+  const reason = core.getInput('reason');
+  console.log(`Heathcheck of duration ${duration}, reason ${reason}`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   core.setOutput("status", "SUCCEEDED");
