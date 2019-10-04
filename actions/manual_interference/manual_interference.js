bdccccd53f3f9a8b12dee04e5535574d5b3195e4
Keen <Keenwby@users.noreply.github.com>
2019-10-04 14:28:18 -0700

    Update and rename healthcheck.js to manual_interference.js

--- actions/manual_interference/manual_interference.js
+++ actions/manual_interference/manual_interference.js
@@ -2,9 +2,8 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const condition = core.getInput('condition');
-  const passingStatus = core.getInput('passing_status');
-  console.log(`Heathcheck of condition $condition, passing_status $passing_status`);
+  const reason = core.getInput('reason');
+  console.log(`Manual interference for reason ${reason}`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   core.setOutput("status", "SUCCEEDED");
