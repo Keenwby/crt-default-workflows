59d1ace45ca02809beab427f0598493d351f97b1
Keen <Keenwby@users.noreply.github.com>
2019-10-09 16:44:41 -0700

    Update ekg.js

--- actions/ekg/ekg.js
+++ actions/ekg/ekg.js
@@ -2,14 +2,15 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+  const target_tuples = core.getInput('target_tuples');
   const criteria = core.getInput('criteria');
-  console.log(`EKG of criteria ${criteria}`);
+  console.log(`EKG of criteria ${criteria} on targets %{target_tuples}`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   core.setOutput("status", "SUCCEEDED");
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
