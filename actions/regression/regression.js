159f19ce5da45e4465007d59d6dd02c027e41daf
Keen <Keenwby@users.noreply.github.com>
2019-10-09 16:32:51 -0700

    Update regression.js

--- actions/regression/regression.js
+++ actions/regression/regression.js
@@ -2,9 +2,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+  const target_tuples = core.getInput('target_tuples');
   const condition = core.getInput('condition');
   const passingStatus = core.getInput('passing_status');
-  console.log(`Regression of condition ${condition}, passing_status ${passingStatus}`);
+  console.log(`Regression of condition ${condition}, passing_status ${passingStatus} on targets ${target_tuples}`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   core.setOutput("status", "SUCCEEDED");
