dd4a11809af2d098106c89f6ed61f8adc6c543c9
Keen <Keenwby@users.noreply.github.com>
2019-10-10 13:23:09 -0700

    Update private_moratorium.js

--- actions/auto_pause/private_moratorium.js
+++ actions/auto_pause/private_moratorium.js
@@ -11,9 +11,9 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   core.setOutput("status", "SUCCEEDED");
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+//   Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
