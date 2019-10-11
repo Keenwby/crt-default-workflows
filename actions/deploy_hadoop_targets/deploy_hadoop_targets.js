73c2d0740c2865fec2d48481d19e13895216455a
bawang <bawang@linkedin.biz>
2019-10-10 17:02:37 -0700

    Add hadoop deploy action

---
+++ actions/deploy_hadoop_targets/deploy_hadoop_targets.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const product = core.getInput('product');
+  const cluster = core.getInput('cluster');
+  const code_version = core.getInput('code_version');
+
+  console.log(`Deploying product ${product} code_version: ${code_version} cluster: ${cluster}`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  core.setOutput("status", "SUCCEEDED");
+  // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
