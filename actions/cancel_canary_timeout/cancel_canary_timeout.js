8c0b85af89acc512cff65d5ea4b6f5916c3b212b
Keen <Keenwby@users.noreply.github.com>
2019-10-14 10:51:26 -0700

    Update cancel_canary_timeout.js

--- actions/cancel_canary_timeout/cancel_canary_timeout.js
+++ actions/cancel_canary_timeout/cancel_canary_timeout.js
@@ -4,7 +4,6 @@ const github = require('@actions/github');
 try {
   const product = core.getInput('product');
   const targets = core.getInput('target_tuples');
-  const requires_manual_check = core.getInput('requires_manual_check');
   console.log(`Cancel canary timeout for product ${product} targets ${targets}`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
